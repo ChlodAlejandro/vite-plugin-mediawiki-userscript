5b6e807627b5411c37cbed4efc09625677e804da
Chlod Alejandro <chlod@chlod.net>
2023-12-04 07:39:50 +0800

    main: use null coalescing assignment ops

--- src/plugin.ts
+++ src/plugin.ts
@@ -5,6 +5,11 @@ import { warn } from './warn';
 import { wrap } from './wrap';
 
 export default function mediawikiUserscript( options: PluginOptions ): Plugin {
+	const externals = Array.from( options.using ?? [] );
+	if ( externals.includes( 'vue' ) ) {
+		externals.push( '@vue' );
+	}
+
 	const cssPlugin = cssInjectedByJsPlugin( {
 		injectCode: ( cssCode ) => {
 			return `mw.util.addCSS(${cssCode});`;
@@ -24,52 +29,47 @@ export default function mediawikiUserscript( options: PluginOptions ): Plugin {
 		enforce: 'post',
 		name: 'mediawiki-userscript',
 		config( config, env ) {
-			if ( !config.build ) {
-				config.build = {};
-			}
-			if ( !config.build.rollupOptions ) {
-				config.build.rollupOptions = {};
-			}
+			config.build ??= {};
+			config.build.rollupOptions ??= {};
 
 			// Insert externals
 			if ( Array.isArray( config.build.rollupOptions.external ) ) {
 				config.build.rollupOptions.external = [
 					...config.build.rollupOptions.external ?? [],
-					...options.using ?? []
+					...externals
 				];
 			} else if ( config.build.rollupOptions.external instanceof Function ) {
 				config.build.rollupOptions.external =
 					( source: string, importer: string | undefined, isResolved: boolean ) => {
 						return ( config.build.rollupOptions.external as Function )(
 							source, importer, isResolved
-						) || options.using?.includes( source );
+						) || externals.includes( source );
 					};
 			} else if ( config.build.rollupOptions.external ) {
 				config.build.rollupOptions.external =
-					[ config.build.rollupOptions.external, ...options.using ?? [] ];
+					[ config.build.rollupOptions.external, ...externals ];
 			} else {
-				config.build.rollupOptions.external = options.using ?? [];
+				config.build.rollupOptions.external = externals;
 			}
 
-			if ( !config.build.target ) {
-				config.build.target = 'es6';
-			}
-			if ( config.build.cssCodeSplit === undefined ) {
-				config.build.cssCodeSplit = true;
-			}
+			config.build.target ??= 'es6';
+			config.build.cssCodeSplit ??= true;
 
 			if ( config.build.lib !== undefined && !options.ignoreBuildOptions ) {
 				warn( 'config.lib already set. Not touching it! Set `ignoreBuildOptions` to `true` to remove this warning.' );
 			} else if ( !config.build.lib ) {
-				const optimizeDepsEntries = config.optimizeDeps?.entries?.[ 0 ];
+				config.optimizeDeps ??= {};
+				const optimizeDepsEntries = config.optimizeDeps.entries?.[ 0 ];
 
+				// Set emitted file
 				config.build.lib = {
 					entry: optimizeDepsEntries ?? options.entry,
 					name: options.name,
 					fileName: options.name,
 					formats: [ 'cjs' ]
 				};
-				// Also disable code splitting
+
+				// Disable code splitting
 				if ( config.build.rollupOptions.output ) {
 					if ( Array.isArray( config.build.rollupOptions.output ) ) {
 						for ( const output of config.build.rollupOptions.output ) {
@@ -88,11 +88,12 @@ export default function mediawikiUserscript( options: PluginOptions ): Plugin {
 
 				if ( !optimizeDepsEntries ) {
 					// Indicate this as the entrypoint of our script
-					if ( !config.optimizeDeps ) {
-						config.optimizeDeps = {};
-					}
 					config.optimizeDeps.entries = [ options.entry ];
 				}
+				config.optimizeDeps.exclude =
+					Array.isArray( config.optimizeDeps.exclude ) ?
+						[ ...config.optimizeDeps.exclude, ...( externals ) ] :
+						externals;
 			}
 
 			getHook( cssPlugin.config ).call( this, config, env );
