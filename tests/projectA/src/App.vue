a3310d4d10c3cee2a28d07e5375b9b6f5db60456
sportshead <me@sportshead.dev>
2024-04-26 21:16:18 +0100

    fix: use composition API in tests/projectA

--- tests/projectA/src/App.vue
+++ tests/projectA/src/App.vue
@@ -1,31 +1,26 @@
 <template>
-	<div id="codex-test-app">
-		<h1>Welcome to Codex</h1>
-		<p>
-			This component will continue to load on all pages; check <code>main.ts</code>
-			to stop this from happening.
-		</p>
-		<p>
-			Test
-		</p>
-		<cdx-button action="progressive" weight="primary">
-			Click me!
-		</cdx-button>
-	</div>
+    <div id="codex-test-app">
+        <h1>Welcome to Codex</h1>
+        <p>
+            This component will continue to load on all pages; check
+            <code>main.ts</code>
+            to stop this from happening.
+        </p>
+        <CdxButton action="progressive" weight="primary" @click="counter++">
+            Click me! Count: {{ counter }}
+        </CdxButton>
+    </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import { CdxButton } from '@wikimedia/codex';
+<script setup lang="ts">
+import { ref } from "vue";
+import { CdxButton } from "@wikimedia/codex";
 
-export default defineComponent( {
-	components: { CdxButton }
-} );
-
+const counter = ref(0);
 </script>
 
 <style scoped>
 #codex-test-app {
-  margin: 64px;
+    margin: 64px;
 }
 </style>
--- tests/projectA/src/main.ts
+++ tests/projectA/src/main.ts
@@ -1,11 +1,12 @@
-import { createApp } from 'vue';
-import App from './App.vue';
+import { createApp } from "vue";
+import App from "./App.vue";
 
-if ( !document.getElementById( 'app' ) ) {
-	const appMount = document.createElement( 'div' );
-	appMount.setAttribute( 'id', 'app' );
-	document.querySelector( '#mw-content-text' )
-		?.insertAdjacentElement( 'beforebegin', appMount );
+if (!document.getElementById("app")) {
+    const appMount = document.createElement("div");
+    appMount.setAttribute("id", "app");
+    document
+        .querySelector("#mw-content-text")
+        ?.insertAdjacentElement("beforebegin", appMount);
 }
 
-createApp( App ).mount( '#app' );
+createApp(App).mount("#app");
