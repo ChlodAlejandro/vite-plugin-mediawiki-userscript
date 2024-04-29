c1a95e6073bdb28051173ec58d440e37f7e3ee0d
sportshead <me@sportshead.dev>
2024-04-29 21:41:16 +0100

    feat!: convert package to esm

--- src/wrap.ts
+++ src/wrap.ts
@@ -1,25 +1,30 @@
 import { readFileSync } from 'node:fs';
-import { resolve } from 'node:path';
-import { PluginOptions } from './config';
+import { resolve, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { PluginOptions } from './config.js';
 
 export function wrap( options: PluginOptions, code: string ): string {
-	const template = options.template ??
+	const template =
+		options.template ??
+		readFileSync(
+			resolve(
+				dirname( fileURLToPath( import.meta.url ) ),
+				'../assets/userscript.js'
+			)
+		).toString( 'utf8' );
 
-		readFileSync( resolve( __dirname, '../assets/userscript.js' ) )
-			.toString( 'utf8' );
-
-	const header = options.banner
-		?.split( '\n' )
-		?.map( v => `// ${v}`.trim() )
-		?.join( '\n' )
-		?.trimEnd() ??
-		'';
-	const footer = options.footer
-		?.split( '\n' )
-		?.map( v => `// ${v}`.trim() )
-		?.join( '\n' )
-		?.trimStart() ??
-		'';
+	const header =
+		options.banner
+			?.split( '\n' )
+			?.map( ( v ) => `// ${v}`.trim() )
+			?.join( '\n' )
+			?.trimEnd() ?? '';
+	const footer =
+		options.footer
+			?.split( '\n' )
+			?.map( ( v ) => `// ${v}`.trim() )
+			?.join( '\n' )
+			?.trimStart() ?? '';
 
 	const processedTemplate = template
 		.replace( /'modules'/g, JSON.stringify( options.using, null, '\t' ) )
@@ -27,12 +32,11 @@ export function wrap( options: PluginOptions, code: string ): string {
 		.replace( /'footer';(\r\n$|\n$)?/g, footer );
 
 	if ( Array.from( processedTemplate.matchAll( /'script'/g ) ).length !== 1 ) {
-		throw new Error( "Userscript template does not have exactly one 'script' tag." );
+		throw new Error(
+			"Userscript template does not have exactly one 'script' tag."
+		);
 	}
 
-	const emitSplit = processedTemplate.split(
-		/^\s+'script';/m,
-		2
-	);
+	const emitSplit = processedTemplate.split( /^\s+'script';/m, 2 );
 	return `${emitSplit[ 0 ]}\n${code}\n${emitSplit[ 1 ]}`;
 }
